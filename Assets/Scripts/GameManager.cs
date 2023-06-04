49f17229da57a833f23c644c3514bc486ee50a8c
ivalchanka <i.valchenk@hotmail.com>
2023-06-04 19:58:34 +0400

    Pushed project skeleton.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private const float SpawnRate = 1;
+
+    public List<GameObject> targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(SpawnRate);
+            int randomIndex = Random.Range(0, this.targets.Count - 1);
+            Instantiate(this.targets[randomIndex]);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private const float MinSpeed = 8.0f;
+    private const float MaxSpeed = 14.0f;
+    private const float TorqueRange = 10.0f;
+    private const float XRange = 4.0f;
+    private const float YSpawnPosition = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.AddForce(this.GetRandomForceVector(), ForceMode.Impulse);
+        rigidBody.AddTorque(this.GetRandomTorque(), this.GetRandomTorque(), this.GetRandomTorque(), ForceMode.Impulse);
+        transform.position = this.GetRandomSpawnPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GetRandomForceVector()
+    {
+        return Vector3.up * Random.Range(MinSpeed, MaxSpeed);
+    }
+
+    private float GetRandomTorque()
+    {
+        return Random.Range(-TorqueRange, TorqueRange);
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-XRange, XRange), -YSpawnPosition, 0);
+    }
+}
