03d7df2992d75a6f4b6d4f44b4dd26113cc890f0
ivalchanka <i.valchenk@hotmail.com>
2023-07-01 21:12:11 +0400

    Pushed some changes.

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button _button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
